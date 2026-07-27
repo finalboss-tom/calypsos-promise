64a8459b751a0fb82d1ea8cf655db97d8687cbf4
Tom <109227940+finalboss-tom@users.noreply.github.com>
2026-07-27 03:32:23 -0400

    feat(aster): export proposal contracts

--- packages/aster/src/index.ts
+++ packages/aster/src/index.ts
@@ -1,5 +1,7 @@
 export * from "./authority.js";
+export * from "./proposal.js";
 export * from "./role-contracts.js";
+export * from "./validate-proposal.js";
 export * from "./validate-role-contracts.js";
 export * from "./validate.js";
 export * from "./version.js";
